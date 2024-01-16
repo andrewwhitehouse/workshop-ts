e0c8700df15d0756a98229126bd1dac38fdf0118
Andrew Whitehouse <andrew@whitehouse.org.uk>
2024-01-16 10:07:38 +0000

    Adds initial commit, with api-stack.ts

---
+++ cdk/constructs/api-stack.ts
@@ -0,0 +1,27 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+
+export class ApiStack extends cdk.Stack {
+  /**
+   * @param {Construct} scope
+   * @param {string} id
+   * @param {StackProps} props
+   */
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const lambdaFunction = new lambda.Function(this, 'HandlerFunction', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'handler.hello',
+      code: lambda.Code.fromAsset('functions'),
+    })
+
+    const api = new apigw.RestApi(this, 'MyApi')
+
+    const lambdaIntegration = new apigw.LambdaIntegration(lambdaFunction)
+    api.root.addMethod('GET', lambdaIntegration)
+  }
+}
+
+module.exports = { ApiStack }
