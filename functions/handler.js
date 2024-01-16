89d2af2ff639975e4177f6d236c809f94c346225
Andrew Whitehouse <andrew@whitehouse.org.uk>
2024-01-16 13:01:00 +0000

    Changes handler message

--- functions/handler.js
+++ functions/handler.js
@@ -3,6 +3,6 @@ module.exports.hello = async function(event) {
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/plain" },
-    body: `Hello, CDK! You've hit ${event.path}\n`
+    body: `Hello JS/TS Hybrid World`
   };
 };
